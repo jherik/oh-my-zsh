1e3ab317caeaa9d29b9fe2e2023aaf354b2b9f1a
Cody Krieger <cody@codykrieger.com>
2012-03-03 03:25:44 -0500

    Add gd for 'git diff' and 'cdff' for a neat-o AppleScript function.

--- lib/functions.zsh
+++ lib/functions.zsh
@@ -15,3 +15,17 @@ function take() {
   cd $1
 }
 
+if [[ "$(uname)" == "Darwin" ]] ; then
+  # get path of frontmost finder window
+  function ff { osascript -e 'tell application "Finder"'\
+   -e "if (${1-1} <= (count Finder windows)) then"\
+   -e "get POSIX path of (target of window ${1-1} as alias)"\
+   -e 'else' -e 'get POSIX path of (desktop as alias)'\
+   -e 'end if' -e 'end tell'; };\
+
+  # cd to path of frontmost finder window
+  function cdff { cd "`ff $@`"; };
+fi
+
+function git(){hub "$@"}
+
--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -42,6 +42,7 @@ alias gm='git merge'
 compdef _git gm=git-merge
 alias gsu='git submodule'
 alias gf='git fetch'
+alias gd='git diff'
 alias grh='git reset HEAD'
 alias grhh='git reset HEAD --hard'
 
